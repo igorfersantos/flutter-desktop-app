e38048371a484cad62ef5a593ac0d3748a4c07d9
Igor <igorfercontato@gmail.com>
2020-10-06 16:50:13 -0300

    First commit

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '38d230a867290a963079';
+const githubClientSecret = 'e18127e8482dced2a1e2a8e8f127504107fd10a3';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
